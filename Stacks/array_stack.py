0632e7f2a22075ce63d869b395290c9cea1d6ef7
Sathvika PN <sathvikapn@gmail.com>
2021-04-13 12:56:58 +0530

    stack implementation with lists for LIFO operation

---
+++ Stacks/array_stack.py
@@ -0,0 +1,30 @@
+class ArrayStack():
+    """ LIFO implementation using Python Lists as underlying storage for stack """
+
+    def __init__(self):
+        """ Initialize stack """
+        self._stack = []
+
+    def is_empty(self):
+        return self._stack == []
+
+    def top(self):
+        if self.is_empty():
+            raise Exception('Empty stack')
+        return self._stack[-1]
+
+    def __len__(self):
+        return len(self._stack)
+
+    def push(self,e):
+        self._stack.append(e)
+    
+    def pop(self):
+        if self.is_empty():
+            raise Exception('Empty stack error')
+        else:
+            return self._stack.pop()
+
+if __name__=='__main__':
+    pass
+

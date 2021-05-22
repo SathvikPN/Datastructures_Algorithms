de1115bfac476e30cfeac7e5e3e6f6a34b67c2a3
Sathvik PN <sathvikapn@gmail.com>
2021-05-22 12:43:51 +0530

    update

---
+++ Algorithms/Sieve_of_Eratosthenes.py
@@ -0,0 +1,6 @@
+# Sieve of Eratosthenes:
+# for finding all the prime numbers in a segment [1:n] using O(nloglogn) operations.
+# A number is prime, if none of the smaller prime numbers divides it
+
+# Read: https://cp-algorithms.com/algebra/sieve-of-eratosthenes.html
+
